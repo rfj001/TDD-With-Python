aa6311e7f1374bf88d629b07c4a607ec1b12c02a
Robert Johnson <rfj001@bucknell.edu>
2014-09-28 22:15:46 -0400

    Basic view now return minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from lists.views import home_page # Function we write in views.py
 
 class HomePageTest(TestCase):
@@ -11,4 +13,21 @@ class HomePageTest(TestCase):
         # Here we check that resolve called with '/' finds a 
         # function called home_page
         self.assertEqual(found.func, home_page)
+        
+    def test_home_page_returns_correct_html(self):
+        # Create HttpRequest object, which is what Django sees when
+        # user asks for a page
+        request = HttpRequest()
+        # Pass request to our home_page view, giving us a response.
+        # Response is of type HttpResponse
+        response = home_page(request)
+        # Assert that the content of the response has certain
+        # properties
+        # Note: response.content is raw bytes, not Python string
+        # so we need to use b'' syntax to compare
+        self.assertTrue(response.content.startswith(b'<html>'))
+        # Since we specify this criteria in our functional test, we should
+        # check it here in our view's unit test
+        # Unit test is driven by the functional test
+        self.assertIn(b'<title>To-Do lists</title>', response.content)    
         
