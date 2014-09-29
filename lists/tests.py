f43e9e1c5581d166d073ec22fee093d099de66f2
Robert Johnson <rfj001@bucknell.edu>
2014-09-28 23:38:26 -0400

    Always commit before refactoring

--- lists/tests.py
+++ lists/tests.py
@@ -29,5 +29,22 @@ class HomePageTest(TestCase):
         # Since we specify this criteria in our functional test, we should
         # check it here in our view's unit test
         # Unit test is driven by the functional test
-        self.assertIn(b'<title>To-Do lists</title>', response.content)    
+        self.assertIn(b'<title>To-Do lists</title>', response.content)
+        
+    def test_home_page_can_save_a_POST_request(self):
+        # Setup
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        
+        # Exercise
+        response = home_page(request)
+        
+        # Assert
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text' : 'A new list item'}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
         
