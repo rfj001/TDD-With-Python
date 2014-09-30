62303b0b5281d8e00133ff4b2256dae98fb88889
Robert Johnson <rfj001@bucknell.edu>
2014-09-30 15:03:56 -0400

    Moved FTs into their own individual files

---
+++ functional_tests/base.py
@@ -0,0 +1,40 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+import sys
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+    
+    # Class method similar to setUp, but only executed once, rather
+    # than before each test method
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            # Look for liveserver command-line argument
+            if 'liveserver' in arg:
+                # If we find it, tell test class to skip the normal setUpClass
+                # and just store away our staging server URL in a variable
+                # server_url instead
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+    
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+    
+    def setUp(self): # setUp and tearDown are run before and after each test
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3) # Tells selenium to wait a few seconds if it needs to, so that pages can complete loading before it tries to do anything
+    
+    def tearDown(self):
+        self.browser.quit()
+        
+    # Note: does not start with test_, so will not be run as a test.
+    # Solely for my own purposes
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -1,43 +1,8 @@
-import sys
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(StaticLiveServerTestCase):
-    
-    # Class method similar to setUp, but only executed once, rather
-    # than before each test method
-    @classmethod
-    def setUpClass(cls):
-        for arg in sys.argv:
-            # Look for liveserver command-line argument
-            if 'liveserver' in arg:
-                # If we find it, tell test class to skip the normal setUpClass
-                # and just store away our staging server URL in a variable
-                # server_url instead
-                cls.server_url = 'http://' + arg.split('=')[1]
-                return
-        super().setUpClass()
-        cls.server_url = cls.live_server_url
-    
-    @classmethod
-    def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
-            super().tearDownClass()
-    
-    def setUp(self): # setUp and tearDown are run before and after each test
-        self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3) # Tells selenium to wait a few seconds if it needs to, so that pages can complete loading before it tries to do anything
-    
-    def tearDown(self):
-        self.browser.quit()
-        
-    # Note: does not start with test_, so will not be run as a test.
-    # Solely for my own purposes
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+class NewVisitorTest(FunctionalTest):
         
     def test_can_start_a_list_and_retrieve_it_later(self): # Any method whose name starts with test is a test method, and is run by the test runner. Descriptive names are good, too.
         # Edith has heard about a cool new online to-do app. She goes to
@@ -109,28 +74,3 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep.
-        
-    def test_layout_and_styling(self):
-        # Edith goes to the home page
-        self.browser.get(self.server_url)
-        self.browser.set_window_size(1024, 768)
-
-        # She notices the input box is nicely centered
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5,
-        )
-        
-        # She starts a new list and sees the input is nicely
-        # centered there too
-        inputbox.send_keys('testing\n')
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2,
-            512,
-            delta=5,
-        )
-    
-    
