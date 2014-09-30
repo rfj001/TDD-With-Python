b4df9217286556218d29a7c530fa32a2216fe8cd
Robert Johnson <rfj001@bucknell.edu>
2014-09-30 17:27:14 -0400

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,32 +6,19 @@ from django.http import HttpRequest
 
 from lists.models import Item, List
 from lists.views import home_page # Function we write in views.py
+from lists.forms import ItemForm
 
 class HomePageTest(TestCase):
-    
-    def test_root_url_resolves_to_home_page_view(self):
-        # django uses resolve to resolve URLS and find what
-        # functions they should map to.
-        found = resolve('/') 
-        # Here we check that resolve called with '/' finds a 
-        # function called home_page
-        self.assertEqual(found.func, home_page)
         
-    def test_home_page_returns_correct_html(self):
-        # Create HttpRequest object, which is what Django sees when
-        # user asks for a page
-        request = HttpRequest()
-        # Pass request to our home_page view, giving us a response.
-        # Response is of type HttpResponse
-        response = home_page(request)
-        # Assert that the content of the response has certain
-        # properties
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
-        # Since we specify this criteria in our functional test, we should
-        # check it here in our view's unit test
-        # Unit test is driven by the functional test
-        self.assertIn(b'<title>To-Do lists</title>', response.content)
+    def test_home_page_renders_home_template(self):
+        response = self.client.get('/')
+        # Assert that we are using the right template
+        self.assertTemplateUsed(response, 'home.html')
+        
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        # Assert that our view is using the right kind of form
+        self.assertIsInstance(response.context['form'], ItemForm)
         
 class ListViewTest(TestCase):
     
