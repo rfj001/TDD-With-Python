5b61247e4b30899dfd0f808f149379485006db5d
Robert Johnson <rfj001@bucknell.edu>
2014-09-30 23:40:04 -0400

    Migrations?

---
+++ lists/static/list.js
@@ -0,0 +1,7 @@
+// Find all input elements, and for each, attach an event listener which
+// reacts to on keypress events and click 
+$(document).ready(function($) {
+	$('#task_input').on('keypress click', function () {
+		$('.has-error').hide();
+	});
+});
