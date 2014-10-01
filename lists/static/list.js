62bb9cddc6b28561bdd7ee36c6995882e32c4c57
Robert Johnson <rfj001@bucknell.edu>
2014-10-01 00:00:34 -0400

    Fixed JavaScript bug

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,7 +1,7 @@
 // Find all input elements, and for each, attach an event listener which
 // reacts to on keypress events and click 
 $(document).ready(function($) {
-	$('#task_input').on('keypress click', function () {
+	$('#id_text').on('keypress click', function () {
 		$('.has-error').hide();
 	});
 });
