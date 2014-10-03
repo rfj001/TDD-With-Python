dd07705090a7a3b8371e328f6532e8fa714be5ee
Robert Johnson <rfj001@bucknell.edu>
2014-10-02 23:49:44 -0400

    placeholder test_my_lists and move login checkers into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -41,3 +41,14 @@ class FunctionalTest(StaticLiveServerTestCase):
         
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+        
+    def wait_to_be_logged_in(self, email):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
