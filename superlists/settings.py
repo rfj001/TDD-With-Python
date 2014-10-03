951e99f35d44825e1ee035219cf7cbbdaab7e7ab
Robert Johnson <rfj001@bucknell.edu>
2014-10-02 22:31:12 -0400

    Fixed small issue

--- superlists/settings.py
+++ superlists/settings.py
@@ -27,6 +27,24 @@ TEMPLATE_DEBUG = DEBUG
 # Needed when DEBUG=False
 ALLOWED_HOSTS = ('staging.robertfjohnson.com',)
 
+## Tells Django to log error messages to the console.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
 
 # Application definition
 
@@ -57,7 +75,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 AUTH_USER_MODEL = 'accounts.User'
 AUTHENTICATION_BACKENDS = (
-    'accounts.authentication.PersonaAuthenticationBackend'
+    'accounts.authentication.PersonaAuthenticationBackend',
 )
 
 
